ca2e645e5153b016d54f61e6ff495d6c21c1e035
khairulumam92 <khairulumamku92@gmail.com>
2025-03-13 12:41:10 +0700

    add App class and initial README files for multiple modules

---
+++ Praktikum Struktur Data/Modul 2/Codelab 1/src/LatinanArrayList.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class LatinanArrayList {
+    public static void main(String[] args) {
+        ArrayList<String> daftarBuah = new ArrayList<>();
+
+        daftarBuah.add("ApeL");
+        daftarBuah.add("Jeruk");
+        daftarBuah.add("Mangga");
+        daftarBuah.add("Semangka");
+
+        System.out.println("Daftar Buah:");
+        for (String buah : daftarBuah) {
+            System.out.println(buah);
+        }
+
+        System.out.println("Elemen pada indeks 1: " + daftarBuah.get(1));
+
+        daftarBuah.set(2, "Durian");
+
+        System.out.println("Daftar Buah setelah perubahan:");
+        for (String buah : daftarBuah) {
+            System.out.println(buah);
+        }
+
+        daftarBuah.remove(3);
+
+        System.out.println("Daftar Buah setelah penghapusan:");
+        for (String buah : daftarBuah) {
+            System.out.println(buah);
+        }
+
+        System.out.println("Jumlah elemen dalam ArrayList: " + daftarBuah.size());
+
+        System.out.println("Apakah ArrayList kosong? " + daftarBuah.isEmpty());
+    }
+}
