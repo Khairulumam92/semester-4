70119f429b4cc6575111dcd9aebd05da8f5c6c4b
khairulumam92 <khairulumamku92@gmail.com>
2025-04-10 12:31:26 +0700

    add initial project setup with VS Code configuration and example classes for stack and queue

---
+++ Praktikum Struktur Data/Modul 3/Latihan 2/src/ContohQueueBaru.java
@@ -0,0 +1,25 @@
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ContohQueueBaru {
+
+    public static void main(String[] args) {
+        Queue<String> hewan = new LinkedList<>();
+
+        hewan.add("Kucing");
+        hewan.add("Burung");
+        hewan.add("Ikan");
+        hewan.add("Gajah");
+        hewan.add("Harimau");
+
+        System.out.println("Peek: " + hewan.peek());
+
+        System.out.println("Daftar Hewan: " + hewan);
+
+        hewan.poll();
+        hewan.poll();
+
+        System.out.println("Daftar Hewan setelah Poll: " + hewan);
+    }
+}
